aac2c6ee3b72eb184d3ac968d3edf2987ed87de2
Pouorix <pouorix@gmail.com>
2022-07-10 16:36:01 +0430

    Add:style for chart

--- src/app/app.index.js
+++ src/app/app.index.js
@@ -7,7 +7,6 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from "recharts";
 
@@ -15,31 +14,42 @@ import "./app.style.scss";
 import { useState } from "react";
 
 function MainApp() {
-  const [data,setData]=useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     get("/getData").then((res) => {
       if (responseValidator(res.status)) {
-        const temp=[];
-        res.data.map((item,index)=>{
-            if(index<10)
-            temp.push({HeartBeat:item.txt.split('-')[2],time:item.txt.split('-')[1]})
-        })
-        console.log(temp)
+        const temp = [];
+        res.data.map((item) => {
+          // if (index < 10)
+            temp.push({
+              HeartBeat: item.txt.split("-")[2],
+              time: item.txt.split("-")[1],
+            });
+            return true;
+        });
+        console.log(temp);
         setData(temp);
       }
     });
   }, []);
   return (
     <div className="embedded-main-app">
-      <h1>Embedded App</h1>
-      <LineChart width={1000} height={400} data={data}>
-        <Line  activeDot={{ r: 8 }} type="monotone" dataKey="HeartBeat" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis  dataKey="time" />
-        <YAxis domain={[10, 160]} />
-        <Tooltip />
-        <CartesianGrid strokeDasharray="3 3" />
-      </LineChart>
+      <ResponsiveContainer width={'100%'} height={'95%'}>
+        <LineChart data={data}>
+          <Line
+            activeDot={{ r: 8 }}
+            type="natural"
+            dataKey="HeartBeat"
+            stroke="#c40d00"
+            strokeWidth={5}
+            dot={{ stroke: 'black', strokeWidth: 2 }}
+          />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis tickMargin={10}   dataKey="time" />
+          <YAxis domain={[10, 160]} />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
