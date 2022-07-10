ddb12178955795286d4583c8e167c654ce4f71ec
Pouorix <pouorix@gmail.com>
2022-07-10 22:54:02 +0430

    Add:header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import './App.css';
 import MainApp from "./app/app.index";
-
 function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/app">
-                    <MainApp/>
-                </Route>
+                
                 <Route path="/" >
-                    <p>khuubi</p>
+                    {/* <Header/> */}
+                   <MainApp/>
                 </Route>
             </Switch>
         </Router>
--- src/app/app.index.js
+++ src/app/app.index.js
@@ -12,6 +12,7 @@ import {
 
 import "./app.style.scss";
 import { useState } from "react";
+import Header from "../header/header";
 
 function MainApp() {
   const [data, setData] = useState([]);
@@ -21,11 +22,11 @@ function MainApp() {
         const temp = [];
         res.data.map((item) => {
           // if (index < 10)
-            temp.push({
-              HeartBeat: item.txt.split("-")[2],
-              time: item.txt.split("-")[1],
-            });
-            return true;
+          temp.push({
+            HeartRate: item.txt.split("-")[2],
+            time: item.txt.split("-")[1],
+          });
+          return true;
         });
         console.log(temp);
         setData(temp);
@@ -34,22 +35,25 @@ function MainApp() {
   }, []);
   return (
     <div className="embedded-main-app">
-      <ResponsiveContainer width={'100%'} height={'95%'}>
-        <LineChart data={data}>
-          <Line
-            activeDot={{ r: 8 }}
-            type="natural"
-            dataKey="HeartBeat"
-            stroke="#c40d00"
-            strokeWidth={5}
-            dot={{ stroke: 'black', strokeWidth: 2 }}
-          />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis tickMargin={10}   dataKey="time" />
-          <YAxis domain={[10, 160]} />
-          <Tooltip />
-        </LineChart>
-      </ResponsiveContainer>
+      <Header/>
+      <div className="chart-container">
+        <ResponsiveContainer width={"100%"} height={"95%"}>
+          <LineChart data={data}>
+            <Line
+              activeDot={{ r: 8 }}
+              type="natural"
+              dataKey="HeartRate"
+              stroke="#c40d00"
+              strokeWidth={5}
+              dot={{ stroke: "black", strokeWidth: 2 }}
+            />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis tickMargin={10} dataKey="time" />
+            <YAxis domain={[10, 160]} />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
